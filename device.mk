1ff2f8bbc8656dd858001e548c93c75dc3a6e27d
droidfivex <droidfivex@gmail.com>
2016-12-04 22:08:50 +0900

    Tuna: doze: Hello doze!
    from hammerheadcaf at e6dffe2b
    
    Change-Id: I008a35d2d4ad85167f2201af1ace34a8c9db6e0f
    
    Conflicts:
    	device.mk
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -47,5 +47,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/releasetools/variant_blobs_hook.sh:install/bin/variant_blobs_hook.sh \
     device/samsung/klte-common/releasetools/variant_blobs.sh:install/bin/variant_blobs.sh
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    TunaDoze
+
 # common klte
 $(call inherit-product, device/samsung/klte-common/klte.mk)
