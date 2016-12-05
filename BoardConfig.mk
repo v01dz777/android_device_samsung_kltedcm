77839cf8f0e97854f6ec128e50023caf434e57de
zen-U <chi.ma7v.i@gmail.com>
2016-12-05 10:17:46 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,15 +18,16 @@
 TARGET_OTA_ASSERT_DEVICE := kltexx,kltelra,kltetmo,kltecan,klteatt,klteub,klteacg,klte,kltekor,klteskt,kltektt
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_eur_defconfig
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/kltedcm/mkbootimg.mk
+TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_dcm_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8974
-TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/klte/init/init_klte.cpp
+TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/kltedcm/init/init_klte.cpp
 TARGET_UNIFIED_DEVICE := true
 
 # NFC
-BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_CHIPSET := sony
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
@@ -36,4 +37,5 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2548039680
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12442369024 # 12442385408 - 16384
 
 # inherit from the proprietary version
+-include vendor/samsung/kltedcm/BoardConfigVendor.mk
 -include vendor/samsung/klte/BoardConfigVendor.mk
