084dbde8a2db341b901fadde634a693958597cee
zen-U <chi.ma7v.i@gmail.com>
2016-12-08 08:13:11 +0900

    Update full_kltedcm.mk

--- full_kltedcm.mk
+++ full_kltedcm.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from klte device
-$(call inherit-product, device/samsung/klte/device.mk)
+$(call inherit-product, device/samsung/kltedcm/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_kltedcm
