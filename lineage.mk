4d4a9010741d2861f305206f1ed292aebf025277
sakuraba <sakura.bar.com@gmail.com>
2017-01-23 19:28:49 +0900

    Rename Product_Name

--- lineage.mk
+++ lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kltedcm/full_kltedcm.mk)
 
 PRODUCT_DEVICE := kltedcm
-PRODUCT_NAME := lineage_klte
+PRODUCT_NAME := lineage_kltedcm
