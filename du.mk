00c94ceaeefdceb27c114dda46ddd16dedb1d464
voidz777 <voidz.evo3d@gmail.com>
2017-05-29 11:42:41 +0900

    kltedcm:DU-ify!

--- du.mk
+++ du.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/kltedcm/full_kltedcm.mk)
 
 PRODUCT_DEVICE := kltedcm
-PRODUCT_NAME := cm_kltedcm
+PRODUCT_NAME := du_kltedcm
