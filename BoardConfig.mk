883cb7dfaba083a5a87c6c73b3b6f033a0c5d786
Bruno Martins <bgcngm@gmail.com>
2017-06-26 16:13:10 -0500

    klte: Abandon TARGET_UNIFIED_DEVICE flag
    * Override the unified properties using vendor init
    
    Change-Id: I71a63cb58cdf3e7c81f3df56e343a3047d6f21b0
    
    Conflicts:
    	BoardConfig.mk
    	init/init_klte.cpp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,6 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_dcm_defconfig
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8974
 TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/kltedcm/init/init_klte.cpp
-TARGET_UNIFIED_DEVICE := true
 
 # NFC
 BOARD_NFC_CHIPSET := sony
