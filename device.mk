9e99bbd6f85d128a5c5cf5749d1af434d832ea73
ma34s <ma34s.sss@gmail.com>
2017-12-14 15:51:43 +0900

    add AOJP configs
    Change-Id: I70515adecd0ee1f36fee74d48b9c697173f08bef

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # AOJP common
-#$(call inherit-product-if-exists, vendor/aojp/config/aojp.mk)
+$(call inherit-product-if-exists, vendor/aojp/config/aojp.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/klte/klte-vendor.mk)
